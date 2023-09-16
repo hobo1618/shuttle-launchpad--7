d7f8c363cdb17682914b523ee67c8d37e43acb0b
Hobo Spaceship <hobdenw@gmail.com>
2023-09-16 22:29:33 +0200

    create and impl trait for Article

--- src/main.rs
+++ src/main.rs
@@ -16,46 +16,69 @@ struct Article {
     published_date: String,
 }
 
+impl SQLStatements<usize> for Article {
+    fn insert(&self) -> String {
+        let mut query_builder: QueryBuilder<Postgres> = 
+            QueryBuilder::new("INSERT INTO articles (title, content, published_date)");
+
+        query_builder.push_values([self], |mut b, article| {
+            b.push_bind(article.title.clone())
+                .push_bind(article.content.clone())
+                .push_bind(article.published_date.clone());
+        });
+        query_builder.into_sql()
+    }
+
+    fn select(key: usize) -> String {
+        format!(
+            "SELECT title, content, published_date FROM articles WHERE id = {}",
+            key
+        )
+    }
+
+}
+
+fn not_found(e: sqlx::Error) -> (StatusCode, String) {
+    (
+        StatusCode::NOT_FOUND,
+        format!("Article with id {} not found", e),
+    )
+}
+
+fn internal_server_error(e: sqlx::Error) -> (StatusCode, String) {
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        format!("Error creating article: {}", e),
+    )
+}
+
+trait SQLStatements<T> {
+    fn insert(&self) -> String;
+    fn select(key: T) -> String;
+}
+
 async fn create_article(
     State(pool): State<PgPool>,
     Json(new_article): Json<Article>,
 ) -> impl IntoResponse {
 
-    let mut query_builder: QueryBuilder<Postgres> = QueryBuilder::new("INSERT INTO articles (title, content, published_date)");
-
-    query_builder.push_values([new_article], |mut b, article| {
-        b.push_bind(article.title)
-            .push_bind(article.content)
-            .push_bind(article.published_date);
-    });
-
-    let result = query_builder.build().execute(&pool).await;
-
-    match result {
-        Ok(_) => (StatusCode::OK, "Article created".to_string()),
-        Err(e) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Error creating article: {}", e.to_string()),
-        ),
-    }
+    sqlx::query(&new_article.insert())
+        .execute(&pool)
+        .await
+        .map(|_| (StatusCode::OK, "Article created successfully".to_string()))
+        .map_err(internal_server_error)
 }
 
 async fn get_article(
     Path(article_id): Path<usize>,
     State(pool): State<PgPool>,
 ) -> Result<Json<Article>, (StatusCode, String)> {
-    let query = format!(
-        "SELECT title, content, published_date FROM articles WHERE id = {}",
-        article_id
-    );
-    let result = sqlx::query_as(&query);
-    let article: Article = result.fetch_one(&pool).await.map_err(|_| {
-        (
-            StatusCode::NOT_FOUND,
-            format!("Article with id {} not found", article_id),
-        )
-    })?;
-    Ok(Json(article))
+
+    sqlx::query_as(&Article::select(article_id))
+        .fetch_one(&pool)
+        .await
+        .map(Json)
+        .map_err(not_found)
 }
 
 
